39cf8c03bcf0746b87654421b8a0367519b59c88
xexiu <sergiu.mironescu@gmail.com>
2023-04-19 16:38:07 +0200

    added more img

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ImageSlider from '@karim04/image-slider';
 
 import image1 from "../src/assets/animal1.jpg";
-import image2 from "../src/assets/animal2.jpg"
+import image2 from "../src/assets/animal2.jpg";
+import image3 from "../src/assets/animal3.jpg";
 
 const body = document.querySelector('body');
-const slider = ImageSlider(image1,image2);
+const slider = ImageSlider(image1, image2, image3);
 body.appendChild(slider);
